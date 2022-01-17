eb4a2048cdbc2b6ac501fe443658a9f48ea25d4e
Suryansh-23 <suryanshchauhan2005@gmail.com>
2022-01-17 21:34:03 +0530

    Hash Routing

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import NavBar from "./components/Navbar.jsx";
 import Main from "./components/Main.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
 import Page404 from "./components/Page404.jsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <NavBar />
             <Routes>
                 <Route path="/" exact element={<Main />} />
                 <Route path="*" element={<Page404 />} />
             </Routes>
             <Footer />
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
